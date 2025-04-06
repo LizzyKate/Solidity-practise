fc1b2e62a8fb64b0028e51042757033030f69b88
LizzyKate <Onyenekwelizabeth@gmail.com>
2025-04-06 12:36:03 +0100

    loop assignment

--- contracts/loop.sol
+++ contracts/loop.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract loopContract {
     uint [] public numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    uint [] public arrayOne = [1, 4, 34, 56];
 
     function checkMultiples(uint _number) public view returns (uint){
         uint count = 0;
@@ -10,6 +11,25 @@ contract loopContract {
             if(numbersList[i] % _number == 0){
                 count++;
             }
+        }
+        return count;
+    }
+
+    function checkIfNumberExists(uint _number) public view returns (bool){
+        for(uint i = 0; i < arrayOne.length; i ++ ){
+           if(_number == arrayOne[i]){
+               return true;
+           }
+        }
+        return false;
+    }
+
+    function numbersThatAreEven() public view returns (uint) {
+        uint count = 0;
+        for(uint i = 0; i < numbersList.length; i ++){
+           if(numbersList[i] % 2 == 0){
+            count++;
+           }
         }
         return count;
     }
